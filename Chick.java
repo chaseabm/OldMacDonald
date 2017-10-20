3e824d2f7c8ac45c9d9b0f1563ae12ec0bc3ea0d
chbush-mclaughlin@s.sfusd.edu <ChBush-Mclaughlin@sfusd.edu>
2017-10-20 10:31:51 -0700

    finishes NamedCow class, fixed Chick class so that variables are initialized in constructor, tweaks Farm class to work properly with changes

--- Chick.java
+++ Chick.java
@@ -2,26 +2,19 @@ class Chick implements Animal
 {     
     private String myType;
     private String mySound;
-/*    public Chick(String type, String sound)
+    public Chick(String type, String sound, String sound2)
     {
-        myType = type;
-        mySound = sound;
-    } */
-    public Chick(String type)
-    {
-        myType = type;
-        mySound = getSound();
+    	myType = type;
+        if (Math.random() < .5)
+        	mySound = sound;
+        else
+        	mySound = sound2;
     }
     public Chick()
     {         
         myType = "unknown";
         mySound = "unknown";
     }
-    public String getSound(){
-    	if (Math.random() < .5)
-        	return "cheep";
-        else
-        	return "cluck";
-    }
+    public String getSound(){return mySound;}
     public String getType(){return myType;}
 }
